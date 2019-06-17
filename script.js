686bb4dbde6982852a1c853b45ebdd14600a8d15
Yurko09 <yurkomits@gmail.com>
2019-06-17 14:34:42 +0300

    WD_PS2 after 1st rewiev:  removed   functions twins for validate email and password, removed alert

--- script.js
+++ script.js
@@ -5,20 +5,20 @@ window.onload = init;
 function init(){
   box_email.addEventListener("blur", validateMail);
   sign_up.addEventListener("click", send);
-  box_password.addEventListener("blur", inputPassword);
+  box_password.addEventListener("blur", validatePassword);
 }
 
 function send(){
   var reg =/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
   var email = document.sign.box_email.value;
   if (!reg.test(email)){
-    alert('Incorrect email.');
+    // alert('Incorrect email.');
     return;
   }
   var pass = document.getElementById('box_password').value;
   var pass_patern = /^\S{8,}$/;
   if (!pass_patern.test(pass)){
-    alert('Password to short.');
+    // alert('Password to short.');
     return;
   }
   var before = document.getElementById('section_before');
@@ -27,7 +27,7 @@ function send(){
   after.classList.remove('invisible');
 }
 
-function inputPassword(){
+function validatePassword(){
   var pass = document.getElementById('box_password').value;
   var pass_patern = /^\S{8,}$/;
   if (!pass_patern.test(pass)){
@@ -38,26 +38,6 @@ function inputPassword(){
   box_password.addEventListener("input", validatePassword);
 }
 
-function validatePassword(){
-  var pass = document.getElementById('box_password').value;
-  var pass_patern = /^\S{8,}$/;
-  if (!pass_patern.test(pass)){
-    document.getElementById('box_password').classList.add('wrong_data');
-  } else {
-    document.getElementById('box_password').classList.remove('wrong_data');
-  }
-}
-
-function validateMailRT(){
-  var mail = document.sign.box_email.value;
-  var reg =/[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/;
-  if (!reg.test(mail)){
-    document.getElementById('box_email').classList.add('wrong_data');
-  } else {
-    document.getElementById('box_email').classList.remove('wrong_data');
-  }
-}
-
 function validateMail(){
   var mail = document.getElementById('box_email').value;
   //expression from https://www.regular-expressions.info/email.html
@@ -65,5 +45,7 @@ function validateMail(){
   if (!reg.test(mail)){
     document.getElementById('box_email').classList.add('wrong_data');
   }
-  document.getElementById('box_email').oninput = validateMailRT;
+  document.getElementById('box_email').oninput = validateMail;
 }
+
+var EMBLEMS = ['arryn.jpg', 'baratheon.jpg', 'greyjoy.jpg', 'lanister.jpg', 'stark.jpg', 'targaryen.jpg'];
